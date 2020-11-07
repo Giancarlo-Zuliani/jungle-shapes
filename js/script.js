c33e86c84e27c6048dcd7dfc604dce72057aa80c
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-07 01:51:42 +0100

    all shapes added, render shapes fixed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,63 @@
+
+
+
+function generateRandomNum(length,maxNum){
+  var arr = [];
+  for(i=0;i < length ; i++){
+  let n = Math.floor(Math.random()* maxNum ) + 1;
+  arr.includes(n) ? i-- : arr.push(n);
+  }
+  return arr
+};
+
+
+function renderShapes(arr,target){
+  if(target === "field"){
+    for(i=0; i < arr.length ;i++){
+      let img = $('<img></img>').attr('src','obj/shape' + arr[i] +'.png');
+      $('section').append(img);
+    }
+  }else if(target === "picked"){
+      for(i=0; i < arr.length;i++){
+        let img = $('<img></img>').attr('src','obj/shape' + arr[i] +'.png');
+        $('header').append(img);
+      }
+    }
+}
+
+renderShapes( generateRandomNum(4,20) , "picked");
+renderShapes( generateRandomNum(20,20) , "field");
+
+$('header').fadeOut(5000,function(){
+    $('section').fadeIn(5000);
+});
+
+var pickedShape=$('header img');
+var targetShape=[];
+
+for(i=0;i<pickedShape.length;i++){
+  let ur = pickedShape[i].src
+  targetShape.push(ur);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  $('section img').click(function(){
    let n = this.src;
-   n = n.slice(-5,-4);
-   n = parseInt(n);
-   console.log(n);
+    targetShape.includes(n) ? console.log('hahi vinto') : console.log('perso')
  })
