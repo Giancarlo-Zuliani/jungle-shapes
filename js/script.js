6397c9adfe240b54cb5994add5fa843eba1d819a
Giancarlo Zuliani <slaigox@gmail.com>
2020-11-09 12:09:42 +0100

    levels difficulty fixed

--- js/script.js
+++ js/script.js
@@ -22,31 +22,77 @@ $('main button').click(function(){
 
 
 $('#level1').click(function(){
-  animateBanner()
+  animateBanner(1)
+})
+$('#level2').click(function(){
+  animateBanner(2)
+})
+$('#level3').click(function(){
+  animateBanner(3)
+})
+$('#level4').click(function(){
+  animateBanner(4)
+})
+$('#level15').click(function(){
+  animateBanner(5)
+})
+$('#level6').click(function(){
+  animateBanner(6)
+})
+$('#level7').click(function(){
+  animateBanner(7)
 })
 
-function animateBanner(){
+
+function animateBanner(n){
   $('.banner').empty()
   $('.banner').css('opacity', '0').css('width' , '100px').css( 'height' , '70px');
   $('.banner').show(100);
-  let banner = $('<h3> Level 1/3  </h3>  <button onclick="levelstart()" id ="levelstart">GO!</button>');
+  let banner = $('<h3> Level'+ n + '</h3>  <button onclick="levelstart(' + n +')" id ="levelstart">GO!</button> <h3>find the shapes!</h3>');
   $('.banner').append(banner);
   $('.banner').animate({
     'width':'590px',
     'height':'400px',
     'opacity' : '1',
   },700,)
-
 }
 
-function levelstart(){
+function levelstart(n){
   $('.banner').hide(300);
-  renderShapes( generateRandomNum(2,20) , "picked");
-  renderShapes( generateRandomNum(20,20) , "field");
-  $('header img').fadeOut(10000,function(){
-    $('section').fadeIn(100000,function(){
+  switch(n){
+    case 1 :
+    renderShapes( generateRandomNum(20,20) , "field");
+    renderShapes( generateRandomNum(2,20) , "picked");
+    break;
+    case 2 :
+    renderShapes( generateRandomNum(30,30) , "field");
+    renderShapes( generateRandomNum(3,30) , "picked");
+    break;
+    case 3 :
+    renderShapes( generateRandomNum(30,30) , "field");
+    renderShapes( generateRandomNum(4,30) , "picked");
+    break;
+    case 4 :
+    renderShapes( generateRandomNum(40,40) , "field");
+    renderShapes( generateRandomNum(5,40) , "picked");
+    break;
+    case 5 :
+    renderShapes( generateRandomNum(50,50) , "field");
+    renderShapes( generateRandomNum(5,50) , "picked");
+    break;
+    case 6 :
+    renderShapes( generateRandomNum(60,60) , "field");
+    renderShapes( generateRandomNum(5,60) , "picked");
+    break;
+    case 7 :
+    renderShapes( generateRandomNum(70,70) , "field");
+    renderShapes( generateRandomNum(6,70) , "picked");
+    break;
+  }
+  $('header img').fadeOut(10000,()=>{
+    $('section').fadeIn(()=>{
       $('section').fadeOut(100000,()=>{
-        animateBanner();
+        $('main').show()
       })
     })
   })
